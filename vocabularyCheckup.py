c8565441890f50babb4e87ee1bf3256c0064a87f
LasseMempel <lassemempel@gmail.com>
2024-10-05 10:14:34 +0200

    broader narrower scheme fix

--- vocabularyCheckup.py
+++ vocabularyCheckup.py
@@ -2,28 +2,6 @@ import lxml.etree
 import glob
 from rdflib import Graph, URIRef, BNode, Literal, Namespace
 from rdflib.namespace import SKOS, RDF, DC, DCTERMS, RDFS
-import random
-
-def idGenerator(size=6, chars= "ABCDFG123456789"):
-    return "".join(random.choice(chars) for _ in range(size))
-
-def createNewId(checkList):
-    x = idGenerator()
-    hasLetters = any(c.isalpha() for c in x)
-    hasNumbers = any(c.isdigit() for c in x)
-    while x in checkList or not hasLetters or not hasNumbers or not x[0].isalpha():
-        x = idGenerator()
-        hasLetters = any(c.isalpha() for c in x)
-        hasNumbers = any(c.isdigit() for c in x)
-    return x
-
-numberOfIds = 10000
-idList = []
-checkList = []
-for i in range(numberOfIds):
-    ID = createNewId(checkList)
-    idList.append(ID)
-    checkList.append(ID)
 
 allRdfFiles = [x for x in glob.glob("*.rdf") if not "modified" in x]
 languageLabel = "@de"
@@ -36,38 +14,24 @@ descriptionDict = {
     "technik_spitzen":{"description":"Systematik für die Technik zur Herstellung von Spitzen und Stickereien", "title":"Spitzentechnik-Systematik", "author":"Sächsische Landesstelle für Museumswesen"},
     }
 
-#conceptDict = {}
-#conceptSchemesDict = {}
-
 generalURI = "https://museumsvokabular-de.github.io/Museumsvokabular-Hub/"
 
 for rdfFile in allRdfFiles:
-    #replace "xml:base="http://www.museumsvokabular.de/museumvok/" in file with generalURI
     with open(rdfFile, 'r', encoding="utf-8") as f:
         text = f.read()
+        #replace "xml:base="http://www.museumsvokabular.de/museumvok/" in file with generalURI
         text = text.replace("xml:base=\"http://www.museumsvokabular.de/museumvok/\"", "xml:base=\""+generalURI+"\"")
-    with open(rdfFile, 'w', encoding="utf-8") as f:
-        f.write(text)
-    # load rdf file with encoding utf-8
-    tree = lxml.etree.parse(rdfFile, parser=lxml.etree.XMLParser(encoding='utf-8'))
+    
+    # load text sting with encoding utf-8
+    root = lxml.etree.fromstring(text.encode("utf-8"))
 
     # find first element with tag "{http://www.w3.org/2004/02/skos/core#}Concept"
-    firstConcept = tree.find("{http://www.w3.org/2004/02/skos/core#}Concept")
+    firstConcept = root.find("{http://www.w3.org/2004/02/skos/core#}Concept")
     scheme = firstConcept.find("{http://www.w3.org/2004/02/skos/core#}inScheme").text
-
-    """
-    # check if scheme is already key in conceptSchemesDict
-    if scheme in conceptSchemesDict:
-        conceptDict[scheme] = idList.pop()
-    schemeURI = URIRef(generalURI + conceptDict[scheme])
-    baseURI = URIRef(generalURI + conceptDict[scheme] + "/")
-    """
 
     schemeURI = URIRef(generalURI + scheme)
 
     print(schemeURI)
-
-    root = tree.getroot()
 
     skosList = ["{http://www.w3.org/2004/02/skos/core#}narrower",
                 "{http://www.w3.org/2004/02/skos/core#}broader"]
@@ -81,10 +45,10 @@ for rdfFile in allRdfFiles:
                 if subElement.tag in skosList:
                     subElement.set("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource", subElement.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource").replace(" ", "_"))
                     if not scheme+"/" in subElement.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource"):
-                        # change tag to broaderMatch or narrowerMatch
-                        subElement.tag = "{http://www.w3.org/2004/02/skos/core#}broadMatch" if subElement.tag == "{http://www.w3.org/2004/02/skos/core#}broader" else "{http://www.w3.org/2004/02/skos/core#}narrowMatch"
+                        wrongScheme = subElement.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource").split("/")[0]
+                        # replace wrong scheme with correct scheme
+                        subElement.set("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource", subElement.get("{http://www.w3.org/1999/02/22-rdf-syntax-ns#}resource").replace(wrongScheme, scheme))
                 if subElement.tag == "{http://www.w3.org/2004/02/skos/core#}inScheme":
-                    #subElement.text = schemeUUID
                     # delete element
                     element.remove(subElement)
             examples = element.findall("{http://www.w3.org/2004/02/skos/core#}example")
@@ -119,9 +83,9 @@ for rdfFile in allRdfFiles:
                 definitions[0].text = definitionText
             elif len(definitions) == 1:
                 definitions[0].text += languageLabel
-    tree.write(scheme+"_modified.rdf", pretty_print=True, encoding="utf-8")
     g = Graph()
-    g.parse(scheme+"_modified.rdf", format="xml", encoding="utf-8")
+    modifiedText = lxml.etree.tostring(root, encoding="utf-8").decode("utf-8")
+    g.parse(data=modifiedText, format="xml", encoding="utf-8")
     g.add ((schemeURI, RDF.type, SKOS.ConceptScheme))
     g.add ((schemeURI, DC.title, Literal(descriptionDict[scheme]["title"])+languageLabel))
     g.add ((schemeURI, DC.description, Literal(descriptionDict[scheme]["description"])+languageLabel))
